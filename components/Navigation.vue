511ea2657aa8bda6f2624493cfb5b9576ef4e359
Camilo Delvasto <camilodelvasto@gmail.com>
2017-09-14 18:10:06 -0500

    More menu fixes

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -37,17 +37,17 @@
           v-on:click="closeMobileMenu()"
         >
           <a to="https://facebook.com/verdeaonline" class="navbar-item" target="_blank">
-            <svg style="width:36px;height:36px" viewBox="0 0 24 24">
+            <svg style="width:28px;height:28px" viewBox="0 0 24 24">
                 <path d="M7.8,2H16.2C19.4,2 22,4.6 22,7.8V16.2A5.8,5.8 0 0,1 16.2,22H7.8C4.6,22 2,19.4 2,16.2V7.8A5.8,5.8 0 0,1 7.8,2M7.6,4A3.6,3.6 0 0,0 4,7.6V16.4C4,18.39 5.61,20 7.6,20H16.4A3.6,3.6 0 0,0 20,16.4V7.6C20,5.61 18.39,4 16.4,4H7.6M17.25,5.5A1.25,1.25 0 0,1 18.5,6.75A1.25,1.25 0 0,1 17.25,8A1.25,1.25 0 0,1 16,6.75A1.25,1.25 0 0,1 17.25,5.5M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9Z" />
             </svg>
           </a>
           <a to="https://facebook.com/verdeaonline" class="navbar-item" target="_blank">
-            <svg style="width:36px;height:36px" viewBox="0 0 24 24">
+            <svg style="width:28px;height:28px" viewBox="0 0 24 24">
                 <path d="M17,2V2H17V6H15C14.31,6 14,6.81 14,7.5V10H14L17,10V14H14V22H10V14H7V10H10V6A4,4 0 0,1 14,2H17Z" />
             </svg>
           </a>
           <a to="https://facebook.com/verdeaonline" class="navbar-item" target="_blank">
-            <svg style="width:36px;height:36px" viewBox="0 0 24 24">
+            <svg style="width:28px;height:28px" viewBox="0 0 24 24">
                 <path d="M10,16.5V7.5L16,12M20,4.4C19.4,4.2 15.7,4 12,4C8.3,4 4.6,4.19 4,4.38C2.44,4.9 2,8.4 2,12C2,15.59 2.44,19.1 4,19.61C4.6,19.81 8.3,20 12,20C15.7,20 19.4,19.81 20,19.61C21.56,19.1 22,15.59 22,12C22,8.4 21.56,4.91 20,4.4Z" />
             </svg>
           </a>
@@ -80,7 +80,7 @@
 
   &.is-active {
     span {
-      background-color: $color-emphasis-alt;
+      background-color: $color-bg-dark;
 
       &:nth-child(1) {
         margin-top: -10px;
@@ -113,7 +113,7 @@
     &.is-active {
       &:hover {
         span {
-          background-color: $color-bg-light;
+          background-color: $color-emphasis;
         }
       }
     }
@@ -135,7 +135,10 @@
 
     .logo-alt {
       display: none;
-      margin: 40px auto;
+
+      @include breakpoint($sm) {
+        margin: 40px auto;
+      }
     }
 
     @include breakpoint($bulma) {
@@ -164,6 +167,14 @@
         display: block;
         animation: appearIn 0.4s;
       }
+
+      > .container {
+        min-height: 35vh;
+
+        @include breakpoint($i6) {
+          min-height: 45vh;
+        }
+      }
     }
   }
 
@@ -178,11 +189,11 @@
     a.navbar-item {
       font-family: $font-secondary;
       text-align: center;
-      font-size: 20px;
+      font-size: 16px;
       text-transform: uppercase;
 
       @include breakpoint($sm) {
-        font-size: 30px;
+        font-size: 24px;
         text-align: center;
       }
 
@@ -217,9 +228,13 @@
     }
 
     .navbar-start {
-      padding-top: 40px;
+      padding-top: 20px;
       display: flex;
       flex-direction: column;
+
+      @include breakpoint($i6) {
+        padding-top: 40px;
+      }
 
       @include breakpoint($sm) {
         padding-top: 80px;
