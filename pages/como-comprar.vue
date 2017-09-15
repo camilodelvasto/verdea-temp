727eb391a58823c51176573098212a813baff14d
Camilo Delvasto <camilodelvasto@gmail.com>
2017-09-15 17:10:18 -0500

    Fix link

--- pages/como-comprar.vue
+++ pages/como-comprar.vue
@@ -10,7 +10,7 @@
           <h2>¿Cómo puedo comprar?</h2>
           <p>Dirígete a <nuxt-link exact to="/">nuestra tienda</nuxt-link>, selecciona la verdea que quieres y dale click en <strong>comprar</strong>. Esto te llevará a nuestro systema de pagos en línea con PayU.</p>
           <h2>¿La llevan a mi casa?</h2>
-          <p>Nosotros entregamos verdeas en Bogotá, y a veces afuera de la ciudad y de Colombia. Puedes <nuxt-link to="//startics.typeform.com/to/MtkSjl">contactarnos</nuxt-link> para saber si podemos llevarlas hasta donde lo necesitas.</p>
+          <p>Nosotros entregamos verdeas en Bogotá, afuera de la ciudad y en ocasiones fuera de Colombia. Puedes <nuxt-link target="_blank" to="//startics.typeform.com/to/MtkSjl">contactarnos</nuxt-link> para saber si podemos llevarlas hasta donde lo necesitas.</p>
         </div>
       </section>
 
