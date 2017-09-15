49a2a14d9122943fe496c8ce3032627e6f491025
Camilo Delvasto <camilodelvasto@gmail.com>
2017-09-15 09:44:56 -0500

    Add contact form

---
+++ pages/contact.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="index contact">
+    <div class="page-wrapper">
+      <Navigation />
+      <ContactForm />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navigation from '~/components/Navigation.vue'
+import ContactForm from '~/components/ContactForm.vue'
+
+export default {
+  components: {
+    Navigation,
+    ContactForm
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
