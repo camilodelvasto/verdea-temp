24078b6a0299cef3c1e8884f1a1d061ab7990e86
Camilo Delvasto <camilodelvasto@gmail.com>
2017-10-04 00:15:29 -0500

    Fix some og missing properties

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,7 +31,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'El encuentro de la naturaleza y el arte' },
       { hid: 'og:url', property: 'og:url', content: 'https://verdea.online/' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
-      { hid: 'og:title', property: 'og:title', content: 'El encuentro de la naturaleza y el arte' },
+      { hid: 'og:title', property: 'og:title', content: 'Verdea: regala vida' },
       { hid: 'og:image', property: 'og:image', content: 'https://res.cloudinary.com/startics/image/upload/v1507093583/favicon_vpshut.png' },
       { hid: 'og:site_name', property: 'og:site_name', content: 'verdea' },
       { hid: 'og:description', property: 'og:description', content: 'El encuentro de la naturaleza y el arte' },
